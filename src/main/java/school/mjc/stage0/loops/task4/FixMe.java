716d85210ff9470c787dfb036a79b2e775ae5a5c
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2023-02-04 20:45:12 +0300

    Update FixMe.java

--- src/main/java/school/mjc/stage0/loops/task4/FixMe.java
+++ src/main/java/school/mjc/stage0/loops/task4/FixMe.java
@@ -3,7 +3,8 @@ public class FixMe {
         for (int i = 0; i < 100; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
-            } else if (i == 7) {
+            } 
+            else if (i == 7) {
                 System.out.println("last");
                 System.out.println("will I be printed?");
                 break;
